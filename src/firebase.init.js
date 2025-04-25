92fe2f90ac1ca4d540660d948d264ad004e4bf4c
SubhaeSadeek <nurun.nabi@outlook.com>
2025-04-25 23:31:41 +0600

    env variables for firebase config done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ4ZBGY-SNNjNCeknRhSVD-zjSaRA7O60",
-  authDomain: "chill-gamer-6eb30.firebaseapp.com",
-  projectId: "chill-gamer-6eb30",
-  storageBucket: "chill-gamer-6eb30.firebasestorage.app",
-  messagingSenderId: "348200533449",
-  appId: "1:348200533449:web:2b630a91abf4eed7e4bc18"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
